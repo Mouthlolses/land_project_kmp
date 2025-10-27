12f20f601302a5e2c3a7b2ef3817fa05c267f383
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-10-27 16:48:59 -0300

    continue_ingre

--- composeApp/src/jvmMain/kotlin/org/francalandproject/project/App.kt
+++ composeApp/src/jvmMain/kotlin/org/francalandproject/project/App.kt
@@ -4,19 +4,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import francalandproject.composeapp.generated.resources.Res
 import francalandproject.composeapp.generated.resources.logofrancasemfundo
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -25,65 +23,91 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App(navController: NavController) {
+
+    val userName = "Matheus"
+    val password = "1234567"
+
     MaterialTheme {
 
         var userInputNameRoot by remember { mutableStateOf("") }
         var userInputPasswordRoot by remember { mutableStateOf("") }
+        val snackBarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
 
-        Column(
-            modifier = Modifier
-                .background(Color.White)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Scaffold(
+            snackbarHost = {
+                SnackbarHost(
+                    hostState = snackBarHostState
+                )
+            }
         ) {
-
-            Image(
-                painter = painterResource(Res.drawable.logofrancasemfundo),
-                contentDescription = null,
+            Column(
                 modifier = Modifier
-                    .height(250.dp)
-            )
-
-            Row(
-                modifier = Modifier
-                    .padding(start = 100.dp, end = 100.dp)
+                    .background(Color.White)
+                    .safeContentPadding()
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                OutlinedTextField(
-                    value = userInputNameRoot,
-                    onValueChange = { userInputNameRoot = it },
-                    label = { Text("Usuário") },
-                    singleLine = true,
-                    shape = RoundedCornerShape(26.dp),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        focusedBorderColor = Color.Gray
-                    ),
-                    modifier = Modifier.width(300.dp)
-                )
-            }
-            Spacer(modifier = Modifier.height(12.dp))
-            Row() {
-                OutlinedTextField(
-                    value = userInputPasswordRoot,
-                    onValueChange = { userInputPasswordRoot = it },
-                    label = { Text("Senha") },
-                    singleLine = true,
-                    shape = RoundedCornerShape(26.dp),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        focusedBorderColor = Color.Gray
-                    ),
-                    modifier = Modifier.width(300.dp)
-                )
-            }
-            Spacer(modifier = Modifier.height(18.dp))
-            Row() {
-                Button(
-                    onClick = { navController.navigate("homeScreen") }
-                ) {
-                    Text("Entrar")
-                }
-            }
 
+                Image(
+                    painter = painterResource(Res.drawable.logofrancasemfundo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(250.dp)
+                )
+
+                Row(
+                    modifier = Modifier
+                        .padding(start = 100.dp, end = 100.dp)
+                ) {
+                    OutlinedTextField(
+                        value = userInputNameRoot,
+                        onValueChange = { userInputNameRoot = it },
+                        label = { Text("Usuário") },
+                        singleLine = true,
+                        shape = RoundedCornerShape(26.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Gray
+                        ),
+                        modifier = Modifier.width(300.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                Row() {
+                    OutlinedTextField(
+                        value = userInputPasswordRoot,
+                        onValueChange = { userInputPasswordRoot = it },
+                        label = { Text("Senha") },
+                        singleLine = true,
+                        shape = RoundedCornerShape(26.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Gray
+                        ),
+                        visualTransformation = PasswordVisualTransformation(),
+                        modifier = Modifier.width(300.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(48.dp))
+                Row() {
+                    Button(
+                        onClick = {
+                            if (userInputNameRoot == userName && userInputPasswordRoot == password) {
+                                navController.navigate("homeScreen")
+                            } else {
+                                scope.launch {
+                                    snackBarHostState.showSnackbar("Usuário ou senha incorretos")
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            .size(width = 240.dp, height = 48.dp),
+                        colors = ButtonDefaults.buttonColors(Color(0xFF1565C0))
+                    ) {
+                        Text("Entrar")
+                    }
+                }
+
+            }
         }
     }
 }
